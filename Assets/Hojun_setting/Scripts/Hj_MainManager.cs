ecdbf7043849582e890cbdf17f39c7e1fc024e72
hk1459 <khk569@nate.com>
2021-08-19 10:31:20 +0900

    vrinit
    I made vrinint scene and new folder.

---
+++ Assets/Hojun_setting/Scripts/Hj_MainManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hj_MainManager : MonoBehaviour
+{
+
+    private static Hj_MainManager instance = null;
+
+    public GameObject Hj_Pointer;
+    public GameObject Hj_Tinnus_object;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
+            instance = this;
+
+            //씬 전환이 되더라도 파괴되지 않게 한다.
+            //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
+            //나는 헷갈림 방지를 위해 this를 붙여주기도 한다.
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            //만약 씬 이동이 되었는데 그 씬에도 Hierarchy에 GameMgr이 존재할 수도 있다.
+            //그럴 경우엔 이전 씬에서 사용하던 인스턴스를 계속 사용해주는 경우가 많은 것 같다.
+            //그래서 이미 전역변수인 instance에 인스턴스가 존재한다면 자신(새로운 씬의 GameMgr)을 삭제해준다.
+            Destroy(this.gameObject);
+        }
+    }
+
+    //게임 매니저 인스턴스에 접근할 수 있는 프로퍼티. static이므로 다른 클래스에서 맘껏 호출할 수 있다.
+    public static Hj_MainManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    public void On_grab_attract()
+    {
+        
+        Hj_Tinnus_object.transform.position = Vector3.Lerp(Hj_Tinnus_object.transform.position, Hj_Pointer.transform.position, Time.deltaTime * 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
